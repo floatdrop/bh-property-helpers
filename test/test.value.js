92cbdf148c1ef266b6614054c07e88902726803e
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-26 22:04:31 +0600

    Added test on overriding in value()

--- test/test.value.js
+++ test/test.value.js
@@ -6,6 +6,20 @@ var Methods = require('..');
 var method = new Methods();
 
 describe('.value()', function () {
+    it('should not override value', function () {
+        var obj = { _tag: 'value' };
+        method(obj).named('tag').changes('_tag').value();
+        obj.tag('newValue');
+        obj.tag().should.equal('value');
+    });
+
+    it('should override value with force flag', function () {
+        var obj = { _tag: 'value' };
+        method(obj).named('tag').changes('_tag').value();
+        obj.tag('newValue', true);
+        obj.tag().should.equal('newValue');
+    });
+
     it('should set value', function () {
         var obj = {};
         method(obj).named('setter').changes('attr').value();
